410806a0ef5626cdb7c10745b0fce2183e79de30
linglong <linglong@cmiot.chinamobile.com>
2022-06-22 18:09:55 +0800

    feat:提交基于Springboot的定时任务demo；

---
+++ src/main/java/com/example/testtimingtask/TestTimingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtimingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTimingTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTimingTaskApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/testtimingtask/service/TimeJobService.java
@@ -0,0 +1,14 @@
+package com.example.testtimingtask.service;
+
+/**
+ * 类 {@code TimeJobService}
+ *
+ * <p>
+ *
+ * @author linglong
+ * @since 2022/6/22
+ **/
+public interface TimeJobService {
+
+    String getString(String param);
+}
